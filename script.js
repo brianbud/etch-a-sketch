129115c05982f8f9f23fbfafb8868b1d2b628b22
Brian Bud <hello@brianbud.com>
2023-10-17 19:08:26 -0400

    add one div box inside container

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("test");
+
+//select container
+//create divs
+//append div to container
+//make sure divs have height and width
+let container = document.querySelector("#container");
+let div = document.createElement("div");
+div.classList.add("box");
+container.appendChild(div);
