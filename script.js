3e442618b1e696f9dacd3dfc728b08bf542865e9
Brian Bud <hello@brianbud.com>
2023-10-17 19:10:31 -0400

    add more than 1 box

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ console.log("test");
 //append div to container
 //make sure divs have height and width
 let container = document.querySelector("#container");
-let div = document.createElement("div");
-div.classList.add("box");
-container.appendChild(div);
+// let div = document.createElement("div");
+// div.classList.add("box");
+// container.appendChild(div);
+
+function createGrids() {
+  for (let i = 0; i < 5; i++) {
+    let div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+  }
+}
+
+createGrids();
